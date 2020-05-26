94a5278a271a7a02bc552ab2fcf76cb0159e8fc8
Nicolas Jannolfo <nicolas.jannolfo@cpe.fr>
2020-05-26 15:41:18 +0200

    Ajout de la gestion de la connexion de l'utilisateur sans le hashage du mdp

--- Atelier-2/Rest-JPA-Mysql-SpringBoot-Sample/src/main/resources/static/visual/js/cardHome.js
+++ Atelier-2/Rest-JPA-Mysql-SpringBoot-Sample/src/main/resources/static/visual/js/cardHome.js
@@ -1,4 +1,11 @@
 $(document ).ready(function(){
+    if(sessionStorage.getItem('user')) {
+        let user = JSON.parse(sessionStorage.getItem('user'));
+        $('#userNameId').text(user.surname);
+    } else {
+        window.location.href = '/visual/login.html';
+    }
+
     $("#playButtonId").click(function(){
         alert("Play button clicked ");
         //TO DO
