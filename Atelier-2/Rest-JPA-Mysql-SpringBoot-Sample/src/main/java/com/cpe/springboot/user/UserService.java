5c316659c2957310da24290ee83010cd71c49aba
Nicolas Jannolfo <nicolas.jannolfo@cpe.fr>
2020-05-26 10:08:58 +0200

    Début implémentation UserService

--- Atelier-2/Rest-JPA-Mysql-SpringBoot-Sample/src/main/java/com/cpe/springboot/user/UserService.java
+++ Atelier-2/Rest-JPA-Mysql-SpringBoot-Sample/src/main/java/com/cpe/springboot/user/UserService.java
@@ -1,4 +1,20 @@
 package com.cpe.springboot.user;
 
+import com.cpe.springboot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public int addUser(User user) {
+
+    }
+
+    public User getUser(int id) {
+
+    }
 }
